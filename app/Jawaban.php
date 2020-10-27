10301665cd3cccc9c6ec8170039b18bed9e67b2f
elviracatrinenatalie <61272072+elviracatrinenatalie@users.noreply.github.com>
2020-10-27 17:57:57 +0700

    Update Jawaban.php

---
+++ app/Jawaban.php
@@ -0,0 +1,17 @@
+
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jawaban extends Model
+{
+    protected $fillable = ['jawaban','user_id','pertanyaan_id'];
+     public function get_users(){
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+    public function get_pertanyaan(){
+        return $this->belongsTo('App\Pertanyaan', 'pertanyaan_id', 'id');
+    }
+}
